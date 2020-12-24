4d37406b6ad7f25880a2370ed694e2c5a64b59e1
Owen Cocjin <owen_jamescf@hotmail.com>
2020-12-24 15:29:39 -0500

    Added stringmisc.py

---
+++ Python/stringmisc.py
@@ -0,0 +1,12 @@
+## Author:  Owen Cocjin
+## Version: 0.1
+## Date:    2020.12.24
+## Description:  Manage the web server's socket connections
+## Notes:
+def ctxt(text, colour=None):
+	'''Add/validate colour to a text. Returns original text on fail'''
+	try:
+		colour=int(colour)
+		return f"\033[{colour}m{text}\033[0m"
+	except:
+		return text
