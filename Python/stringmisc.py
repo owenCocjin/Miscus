527a0e61196888665fc7ebaf629b21e0a6ab486d
Owen Cocjin <owen_jamescf@hotmail.com>
2020-12-24 16:15:01 -0500

    Changed colour variables

--- Python/stringmisc.py
+++ Python/stringmisc.py
@@ -3,13 +3,13 @@
 ## Date:    2020.12.24
 ## Description:  Functions for string manipulation
 ## Notes:
-def ctxt(text, colour=39, bg=49):
-	'''Add/validate colour to a text. Returns original text on fail'''
+def ctxt(text, *, clr=39, bg=49):
+	'''Add/validate clr to a text. Returns original text on fail'''
 	try:
-		colour=int(colour)
+		clr=int(clr)
 		bg=int(bg)
-		if (30<=colour<=39 or 90<=colour<=97)\
+		if (30<=clr<=39 or 90<=clr<=97)\
 		and (40<=bg<=49 or 100<=bg<=107):
-			return f"\033[{colour}m\033[{bg}m{text}\033[0m"
+			return f"\033[{clr}m\033[{bg}m{text}\033[0m"
 	except:
 		return text
