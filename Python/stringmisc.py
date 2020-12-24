271fd07d6477d01bb4acf0f2de507d47685158d2
Owen Cocjin <owen_jamescf@hotmail.com>
2020-12-24 16:03:02 -0500

    Added background

--- Python/stringmisc.py
+++ Python/stringmisc.py
@@ -3,10 +3,13 @@
 ## Date:    2020.12.24
 ## Description:  Functions for string manipulation
 ## Notes:
-def ctxt(text, colour=None):
+def ctxt(text, colour=39, bg=49):
 	'''Add/validate colour to a text. Returns original text on fail'''
 	try:
 		colour=int(colour)
-		return f"\033[{colour}m{text}\033[0m"
-	except:
+		bg=int(bg)
+		if (30<=colour<=39 or 90<=colour<=97)\
+		and (40<=bg<=49 or 100<=bg<=107):
+			return f"\033[{colour}m\033[{bg}m{text}\033[0m"
+	else:
 		return text
