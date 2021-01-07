b175333f5e9aec0dcf4d7cd8a7794c034d10ffff
Owen Cocjin <owen_jamescf@hotmail.com>
2021-01-06 23:21:34 -0500

    Added encodeSpecials

--- Python/htmlparse.py
+++ Python/htmlparse.py
@@ -1,14 +1,28 @@
 ##
 ## Author:  Owen Cocjin
-## Version: 0.1
+## Version: 0.2
 ## Date:    2021.01.04
 ## Description:  Functions for HTML parsing
 ## Notes:
+## Updates:
+##    - Added encodeSpecials
+##    - Added an if check which should make encode/decoders more efficient(?)
 
 def decodePercent(data):
 	'''Decode percent encoding.
 	Assumes all data passed is valid (ie. percent is followed by 2 digits)'''
 	percent_dict={"%26":'&'}
 	for k in percent_dict:
-		data=data.replace(k, percent_dict[k])
+		if data.find(k)!=-1:
+			data=data.replace(k, percent_dict[k])
+	return data
+
+def encodeSpecials(data):
+	'''Encodes special chars within data'''
+	special_dict={'<':"&lt",
+	'>':"&gt",
+	'\n':"</br>"}
+	for k in special_dict:
+		if data.find(k)!=-1:
+			data=data.replace(k, special_dict[k])
 	return data
