aaeac5c3475178610d393b36eaf6012d01783db3
Owen Cocjin <owen_jamescf@hotmail.com>
2021-12-10 13:44:41 -0500

    Moved dicts out of functions

--- Python/htmlparse.py
+++ Python/htmlparse.py
@@ -1,16 +1,24 @@
 ##
 ## Author:  Owen Cocjin
-## Version: 0.4
-## Date:    2021.01.04
+## Version: 0.4.1
+## Date:    2021.12.10
 ## Description:  Functions for HTML parsing
 ## Notes:
 ## Updates:
-##    - Increased space options for &nbsp in encodeSpecial
+##  - Increased space options for &nbsp in encodeSpecial
+##  - Moved dictionaries out of functions
+
+percent_dict={"%26":'&'}
+special_dict={'<':"&lt",
+'>':"&gt",
+'\n':"</br>",
+'\t':"&nbsp&nbsp&nbsp&nbsp",
+"    ":"&nbsp&nbsp&nbsp&nbsp",
+"        ":"&nbsp&nbsp&nbsp&nbsp"}
 
 def decodePercent(data):
 	'''Decode percent encoding.
 	Assumes all data passed is valid (ie. percent is followed by 2 digits)'''
-	percent_dict={"%26":'&'}
 	for k in percent_dict:
 		if data.find(k)!=-1:
 			data=data.replace(k, percent_dict[k])
@@ -18,12 +26,6 @@ def decodePercent(data):
 
 def encodeSpecial(data):
 	'''Encodes special chars within data'''
-	special_dict={'<':"&lt",
-	'>':"&gt",
-	'\n':"</br>",
-	'\t':"&nbsp&nbsp&nbsp&nbsp",
-	"    ":"&nbsp&nbsp&nbsp&nbsp",
-	"        ":"&nbsp&nbsp&nbsp&nbsp"}
 	for k in special_dict:
 		if data.find(k)!=-1:
 			data=data.replace(k, special_dict[k])
